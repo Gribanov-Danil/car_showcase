4764095922fd28f479db416ee0d59a21fcb34346
Danil Gribanov <gribanovwork@yandex.ru>
2023-06-15 19:41:58 +0500

    feat: add image in hero block

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import {Button} from "@/components/button";
+import Image from "next/image";
+import HeroImg from "../public/hero.png"
 
 export const Hero = () => {
     return (
@@ -17,6 +19,12 @@ export const Hero = () => {
                     title="Explore cars"
                     containerStyle="bg-primary-blue text-white rounded-full mt-10"
                 />
+            </div>
+            <div className="hero__image-container">
+                <div className="hero__image">
+                    <Image src={HeroImg} alt="hero" fill className="object-contain"/>
+                    <div className="hero__image-overlay"></div>
+                </div>
             </div>
         </div>
     )
