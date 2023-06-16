185d4459ea0ed68a13d588cc76d1ab88218807ba
Danil Gribanov <gribanovwork@yandex.ru>
2023-06-16 17:46:30 +0500

    feat: add search manufacturer component

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,8 @@ import {Hero} from "@/components/hero";
 import {Button} from "@/components/button";
 import {Navbar} from "@/components/navbar";
 import {Footer} from "@/components/footer";
+import {SearchBar} from "@/components/search-bar";
+import {Filter} from "@/components/filter";
+import {SearchManufacturer} from "@/components/search-manufacturer";
 
-export {Button, Hero,  Navbar, Footer}
+export {Button, Hero,  Navbar, Footer, SearchBar, Filter, SearchManufacturer}
