738944518f1e912164fc73eb7d1b91f51340491a
Danil Gribanov <gribanovwork@yandex.ru>
2023-06-16 17:49:41 +0500

    feat: add props interface

--- components/search-manufacturer.tsx
+++ components/search-manufacturer.tsx
@@ -1,3 +1,10 @@
-export const SearchManufacturer = () => {
+import {Dispatch, FC, SetStateAction} from "react";
+
+interface ISearchManufacturer {
+  manufacturer: string,
+  setManufacturer:  Dispatch<SetStateAction<string>>
+}
+
+export const SearchManufacturer:FC<ISearchManufacturer> = ({manufacturer, setManufacturer}) => {
   return <></>
 }
