e84e8f1c683016d80d86c62dcf357be09dc9e962
Danil Gribanov <gribanovwork@yandex.ru>
2023-07-02 16:34:19 +0500

    feat: add search-bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,23 @@
-import {Hero} from "@/components";
+import {Filter, Hero, SearchBar} from "@/components";
 
 export default function Home() {
-  return (
-    <main className="overflow-hidden">
-      <Hero />
-        <div className="mt-12 padding-x padding-y max-width" id="discover">
-          <div className="home__text-container" >
-            <h2 className="text-4xl font-extrabold">Car Catalogue</h2>
-            <p>Explore the cars you might like</p>
-          </div>
+    return (
+        <main className="overflow-hidden">
+            <Hero/>
+            <div className="mt-12 padding-x padding-y max-width" id="discover">
+                <div className="home__text-container">
+                    <h2 className="text-4xl font-extrabold">Car Catalogue</h2>
+                    <p>Explore the cars you might like</p>
+                </div>
 
-          <div className="home__filters">
-          {/*  SerschBar*/}
-            <div className="home__filter-container">
-            {/*  Custom filter*/}
+                <div className="home__filters">
+                    <SearchBar/>
+                    <div className="home__filter-container">
+                        <Filter/>
+                        <Filter/>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-    </main>
-  )
+        </main>
+    )
 }
